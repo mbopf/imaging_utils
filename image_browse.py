50fde6dcc3ed801362d562553b808ded5a440e7b
Mike Bopf <mbopf@nih.gov>
2022-06-06 18:04:31 -0400

    This version "works" but has paging issues. By default it does handle common image types by extension (jpg, png, tif, dcm).

--- image_browse.py
+++ image_browse.py
@@ -1,37 +1,106 @@
 import argparse
 import os
-import glob
 import sys
-import itertools
+import glob
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pydicom
+from PIL import Image
+from matplotlib.widgets import Button
 
 
-def get_file_list(path: str, wild: str = None) -> list:
-    ret_list = []
-    if not path.endswith('/'):
-        path = path + '/'
-    
-    if wild:
-        for filename in glob.glob(path + wild):
-            ret_list.append(filename)
+PIL_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.tif', '.tiff']
+ALL_EXTENSIONS = PIL_EXTENSIONS + ['.dcm']
+
+# Given a filename, use the extension to determine if it is an image
+def is_img_ext(filename: str) -> bool:
+    return os.path.splitext(filename)[1].lower() in ALL_EXTENSIONS
+
+
+# Give a path and optional glob string, return the list of files in
+# the directory. Also, optionally filter by image extension.
+def get_file_list(path: str, glob_str: str = None, filter_images=True) -> list:
+    if os.path.isfile(path):
+        if glob_str:
+            print('Warning: glob string ignored for single file input')
+        return [path]
+
+    if glob_str:
+        if not path.endswith('/'):
+            path += '/'
+        filelist = glob.glob(path + glob_str)
     else:
-        for filename in os.listdir(path):
-            ret_list.append(filename)
-    
-    return ret_list
+        filelist = os.listdir(path)
+
+    if filter_images:
+        filelist = [file for file in filelist if is_img_ext(file)]
+
+    return filelist
 
 
-# From Alex Chan https://alexwlchan.net/2018/12/iterating-in-fixed-size-chunks/
-def chunked_iterable(iterable, size):
-    it = iter(iterable)
-    while True:
-        chunk = tuple(itertools.islice(it, size))
-        if not chunk:
-            break
-        yield chunk
+def quitit(event):
+    exit(0)
+
+def get_image(filename: str):
+    ext = os.path.splitext(filename)[1].lower()
+    if ext == '.dcm':
+        dicom_data = pydicom.read_file(filename)
+        im = dicom_data.pixel_array
+        im_shape = im.shape
+    elif ext in PIL_EXTENSIONS:
+        im = Image.open(filename)
+        im_shape = im.size
+    else:
+        print(f"file not in {PIL_EXTENSIONS}")
+
+    return im, im_shape
+
+
+class Chunk:
+
+    def __init__(self, filelist, chunk_size, fig, axs):
+        self.chunk_idx = 1
+        self.filelist = filelist
+        self.chunk_size = chunk_size
+        self.fig = fig
+        self.axs = axs
+
+    def get_chunk(self):
+        start = self.chunk_size * (self.chunk_idx - 1)
+        end = self.chunk_size * self.chunk_idx
+        print(f'start: {start}; end: {end}; self.filelist: {self.filelist}')
+        if end > len(self.filelist):
+            end = len(self.filelist)
+
+        return self.filelist[start:end]
+
+    def display_chunk(self):
+        curr_chunk = self.get_chunk()
+        print("In display_chunk():")
+        print(f"axs: {self.axs}; curr_chunk: {curr_chunk}")
+        for ax, file in zip(self.axs.ravel(), curr_chunk):
+            basename = os.path.basename(file)
+            img, im_shape = get_image(file)
+            ax.set_title(basename)
+            ax.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+            ax.set_xlabel(im_shape[1])
+            ax.set_ylabel(im_shape[0])
+            ax.imshow(img, cmap=plt.cm.bone)
+
+        #fig.suptitle(f"Batch {batch_num} of {batches}")
+        self.fig.suptitle(f"Batch batch_num of batches")
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+    def next(self, event):
+        self.chunk_idx +=1
+        self.display_chunk()
+
+    def prev(self, event):
+        self.chunk_idx -=1
+        self.display_chunk()
+
 
 def img_browse():
     """
@@ -58,72 +127,34 @@ def img_browse():
         print(f"ERROR: couldn't open file_path: {file_path}")
         sys.exit()
 
-    if os.path.isfile(file_path):
-        basename = os.path.basename(file_path)
-        dicom_ds = pydicom.read_file(file_path, force=True)
-        if opt.list_dicom:
-            print(dicom_ds)
-        if opt.view_image:
-            fig, ax = plt.subplots()
-            pixel_array = dicom_ds.pixel_array
-            ishape = pixel_array.shape
-            ax.set_title(basename)
-            ax.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
-            ax.set_xlabel(ishape[1])
-            ax.set_ylabel(ishape[0])
-            ax.imshow(pixel_array, cmap=plt.cm.bone)
-            plt.show()
-    else:
-        plt.ion()
-        assert(os.path.isdir(file_path))
-        batch_size = grid_tuple[0] * grid_tuple[1]
-        print(f'batch_size = {batch_size}')
-        batch_num = 1
+    plt.ion()
+    batch_size = grid_tuple[0] * grid_tuple[1]
+    print(f'batch_size = {batch_size}')
 
-        full_list = get_file_list(file_path, opt.glob_str)
-        fsize = (grid_tuple[1] * 6, grid_tuple[0] * 4)
-        print(f'len(full_list) = {len(full_list)}')
-        print(f'fsize = {fsize}')
-        fig, axs = plt.subplots(grid_tuple[0], grid_tuple[1], figsize=fsize)
-        start = batch_size * (batch_num - 1)
-        end = batch_size * batch_num
-        if len(full_list) % batch_size != 0:
-            batches = int(len(full_list)/batch_size) + 1
-        cmd = 'n'
+    full_list = get_file_list(file_path, opt.glob_str)
+    fsize = (grid_tuple[1] * 6, grid_tuple[0] * 4)
+    print(f'len(full_list) = {len(full_list)}')
+    print(f'fsize = {fsize}')
+    fig, axs = plt.subplots(grid_tuple[0], grid_tuple[1], figsize=fsize)
+    plt.subplots_adjust(bottom=0.2)
 
-        file_chunks = chunked_iterable(full_list, batch_size)
-        while cmd != 'q':
-            file_chunk = next(file_chunks)
-            #for ax, file in zip(axs.ravel(), full_list[start:end]):
-            for ax, file in zip(axs.ravel(), file_chunk):
-#                print(batch_num, start, end)
-                basename = os.path.basename(file)
-                dicom_ds = pydicom.read_file(file, force=True)
-                if opt.list_dicom:
-                    print(dicom_ds)
-                pixel_array = dicom_ds.pixel_array
-                ishape = pixel_array.shape
-                ax.set_title(basename)
-                ax.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
-                ax.set_xlabel(ishape[1])
-                ax.set_ylabel(ishape[0])
-                ax.imshow(pixel_array, cmap=plt.cm.bone)
+    callback = Chunk(full_list, batch_size, fig, axs)
+    axprev = plt.axes([0.59, 0.05, 0.1, 0.075])
+    axnext = plt.axes([0.7, 0.05, 0.1, 0.075])
+    axquit = plt.axes([0.81, 0.05, 0.1, 0.075])
+    bprev = Button(axprev, 'Previous')
+    bprev.on_clicked(callback.prev)
+    bnext = Button(axnext, 'Next')
+    bnext.on_clicked(callback.next)
+    bquit = Button(axquit, 'Quit')
+    bquit.on_clicked(quitit)
+    print("before call to 'display_chunk'")
+    callback.display_chunk()
+#    fig.canvas.draw()
+#    fig.canvas.flush_events()
+    plt.ioff()
+    plt.show()
 
-            fig.suptitle(f"Batch {batch_num} of {batches}")
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-            cmd = input('"n" for next batch; "q" to quit->')
-            print(f'cmd: {cmd}')
-            if not cmd.startswith('q') and not cmd.startswith('Q'):
-                if cmd.startswith('n') or cmd.startswith('N'):
-                    batch_num += 1
-#                elif cmd.startswith('b') or cmd.startswith('B'):
-#                    batch_num -= 1
-
-#                start = batch_size * (batch_num - 1)
-#                end = batch_size * batch_num
-#                if end > len(full_list):
-#                    end = len(full_list)
 
 if __name__ == '__main__':
     print(f'Backend: {mpl.get_backend()}')
